7ceab2ed359984a058639014cfe51d9cb2291ffc
CHMA2009 <chamamd2009@gmail.com>
2025-04-28 05:07:09 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -11,7 +11,7 @@ function replaceYouTubeID(url) {
 
 cmd({
     pattern: "play3",
-    alias: ["mp3", "ytmp3","song2"],
+    alias: ["mp5", "ytmp3","song2"],
     react: "🎵",
     desc: "Download Ytmp3",
     category: "download",
